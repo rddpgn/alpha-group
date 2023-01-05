c344139709c76e26d498968cff983a4ed2d87a4e
rddpgn <rddpgn@gmail.com>
2023-01-05 22:37:57 +0300

    Header layout

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import {useEffect} from "react";
 import WelcomeScreen from "../src/screens/WelcomeScreen";
 import WhyScreen from "../src/screens/WhyScreen";
 import PriceScreen from "../src/screens/PriceScreen";
+import Header from "../src/components/Header";
 
 export default function Home() {
     return (
         <>
+            <Header/>
             <div className='page_content'>
                 <WelcomeScreen/>
                 <WhyScreen/>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,32 @@
+import style from "./style.module.css";
+import {cs} from "../../../utils/Utils";
+
 function Header() {
     return (
-        <div>
-            <nav>
-                <img src="" alt=""/>
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
+        <header className={cs(style.header, "flex_h_c_sb")}>
+            <img
+                className={style.img_logo}
+                src="images/header_logo.png"
+                alt="Альфа Групп"
+            />
+            <nav className={cs(style.nav_links, "flex_h_c_sb")}>
+                <a className={style.nav_link} href="">Главная</a>
+                <a className={style.nav_link} href="">О нас</a>
+                <a className={style.nav_link} href="">Цены</a>
+                <a className={style.nav_link} href="">Галерея</a>
+                <a className={style.nav_link} href="">Команда</a>
+                <a className={style.nav_link} href="">Взаимодействие</a>
+                <a className={style.nav_link} href="">Контакты</a>
             </nav>
-        </div>
+            <div className={style.location_wrapper}>
+                <span className={style.location_label}>Местоположение</span>
+                <button className={style.location_button}>Санкт-Петербург</button>
+            </div>
+            <div className={style.phone_wrapper}>
+                <span className={style.phone_number}>8 812 448 08 09</span>
+                <button className={style.phone_label}>Обратный звонок </button>
+            </div>
+        </header>
     )
 }
 
